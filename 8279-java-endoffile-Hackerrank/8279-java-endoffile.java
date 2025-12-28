fd5880e6b5d15666755b801c09f0611cf1fe549b
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2025-12-28 13:35:05 +0530

    Added solution - LeetHub

---
+++ 8279-java-endoffile-Hackerrank/8279-java-endoffile.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int c=1;
+        while(sc.hasNext()){
+            String s1=sc.nextLine();
+            System.out.println(c+" "+s1);
+            c++;
+        }
+        // String s1=sc.nextLine();
+        // String s2=sc.nextLine();
+        // String s3=sc.nextLine();
+    //     System.out.println("2 "+s2);
+    //     System.out.println("3 "+s3);
+     }
+}
