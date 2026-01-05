157b7ce117e2d5a71df09aad97ed79595a1b5749
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-01-05 20:53:16 +0530

    Added solution - LeetHub

---
+++ 8654-plus-minus-Hackerrank/8654-plus-minus.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+    int p=0,n=0,z=0;
+      for(int i=0;i<arr.size();i++){
+         if(arr.get(i)>0){
+            p++;
+         }else if(arr.get(i)<0){
+            n++;
+         }else{
+            z++;
+         }
+      }
+      int a=arr.size();
+      float a1=(float)p/a;
+      float a2=(float)n/a;
+      float a3=(float)z/a;
+      System.out.printf("%.6f",a1);
+      System.out.println();
+      System.out.printf("%.6f",a2);
+      System.out.println();
+      System.out.printf("%.6f",a3);
+      System.out.println();
+      
+      
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
