c4957dd01341ea0c71e36138d0c76f5ee45ae86b
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-03-21 16:58:02 +0530

    Time: 0 ms (100%), Space: 42.9 MB (67.43%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,33 +10,42 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-         ListNode temp=head;
-        int c=0;
-        while(temp!=null){
-            c++;
-            temp=temp.next;
-        }
+        //  ListNode temp=head;
+        // int c=0;
+        // while(temp!=null){
+        //     c++;
+        //     temp=temp.next;
+        // }
 
-        int k=0;
-        if(c%2==1){
-            k=c/2;
-            k+=1;
-        }else{
-            k=c/2;
-            k+=1;
-        }
-        ListNode curnode=null;
-        ListNode tempp=head;
-        int curpos=1;
-        while(tempp!=null){
-            if(curpos==k){
-                curnode=tempp;
-                break;
-            }
-            curpos++;
-            tempp=tempp.next;
-        }
-        return curnode;
+        // int k=0;
+        // if(c%2==1){
+        //     k=c/2;
+        //     k+=1;
+        // }else{
+        //     k=c/2;
+        //     k+=1;
+        // }
+        // ListNode curnode=null;
+        // ListNode tempp=head;
+        // int curpos=1;
+        // while(tempp!=null){
+        //     if(curpos==k){
+        //         curnode=tempp;
+        //         break;
+        //     }
+        //     curpos++;
+        //     tempp=tempp.next;
+        // }
+        // return curnode; 
+     // TWO-POINTERS
+    ListNode slow=head;
+    ListNode fast=head;
+    while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    return slow;
+
 
     }
 }
