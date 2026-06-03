a1133691d3c93d191ffc678a67a9d9d4cf23333d
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-03 08:35:31 +0530

    Added solution - LeetHub

---
+++ 702788-sorted-and-rotated-minimum-GFG/702788-sorted-and-rotated-minimum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMin(int[] arr) {
+        // code here
+        int low=0,high=arr.length-1;
+        int ans=Integer.MAX_VALUE;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[low]<=arr[mid]){
+                ans=Math.min(ans,arr[low]);
+                low=mid+1;
+            }else{
+                 ans=Math.min(ans,arr[mid]);
+                 high=mid-1;
+            }
+        }
+        return ans;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
