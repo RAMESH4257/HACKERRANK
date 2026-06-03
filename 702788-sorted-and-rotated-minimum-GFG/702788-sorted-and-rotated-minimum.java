e9054210605e417654e847dc59327768fd195015
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-03 08:39:27 +0530

    Added solution - LeetHub

--- 702788-sorted-and-rotated-minimum-GFG/702788-sorted-and-rotated-minimum.java
+++ 702788-sorted-and-rotated-minimum-GFG/702788-sorted-and-rotated-minimum.java
@@ -5,6 +5,10 @@ class Solution {
         int ans=Integer.MAX_VALUE;
         while(low<=high){
             int mid=(low+high)/2;
+            if(arr[low]<=arr[high]){
+                ans = Math.min(ans, arr[low]);
+                break;
+            }
             if(arr[low]<=arr[mid]){
                 ans=Math.min(ans,arr[low]);
                 low=mid+1;
