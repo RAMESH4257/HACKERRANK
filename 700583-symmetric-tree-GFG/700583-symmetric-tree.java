86e4b7ba3e684adf1b6c32ab5de192e4ab06aa7b
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-16 11:24:32 +0530

    Added solution - LeetHub

--- 700583-symmetric-tree-GFG/700583-symmetric-tree.java
+++ 700583-symmetric-tree-GFG/700583-symmetric-tree.java
@@ -12,26 +12,35 @@ class Node{
 */
 
 class Solution {
+    public boolean check(Node n1,Node n2){
+        if(n1==null && n2==null) return true;
+        if(n1==null||n2==null) return false;
+        if(n1.data!=n2.data) return false;
+        return check(n1.left,n2.right)&&check(n1.right,n2.left);
+    }
     public boolean isSymmetric(Node root) {
-        // code here
-         if(root==null){
+        if(root==null){
             return false;
         }
-        Queue<Node> q=new LinkedList<>();
-        q.add(root.left);
-        q.add(root.right);
-        while(!q.isEmpty()){
-            Node n1=q.remove();
-            Node n2=q.remove();
-            if(n1==null && n2==null) continue;
-            if(n1==null || n2==null) return false;
-            if(n1.data!=n2.data) return false;
-            q.add(n1.left);
-            q.add(n2.right);
-            q.add(n1.right);
-            q.add(n2.left);
-        }
-        return true;
+        return check(root.left,root.right);
+        //  if(root==null){
+        //     return false;copy
+        // }
+        // Queue<Node> q=new LinkedList<>();
+        // q.add(root.left);
+        // q.add(root.right);
+        // while(!q.isEmpty()){
+        //     Node n1=q.remove();
+        //     Node n2=q.remove();
+        //     if(n1==null && n2==null) continue;
+        //     if(n1==null || n2==null) return false;
+        //     if(n1.data!=n2.data) return false;
+        //     q.add(n1.left);
+        //     q.add(n2.right);
+        //     q.add(n1.right);
+        //     q.add(n2.left);
+        // }
+        // return true;
     }
 }
 
