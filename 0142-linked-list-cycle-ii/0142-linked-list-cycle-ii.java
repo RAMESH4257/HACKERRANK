3d1088c58b6c6f252cf1b7ea491fca0a62e26e1c
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-19 12:34:24 +0530

    Time: 0 ms (100%), Space: 46.5 MB (69.4%) - LeetHub - Time: O(n), Space: O(1)

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -14,15 +14,33 @@ public class Solution {
         if( head==null || head.next==null){
             return null;
         }
-        ListNode temp=head;
-        HashSet<ListNode> hs=new HashSet<>();
-        while(temp!=null){
-           if(hs.contains(temp)){
-             return temp;
-           }
-           hs.add(temp);
-           temp=temp.next;
+        ListNode slow=head,fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+               slow=head;
+               while(slow!=fast){
+                slow=slow.next;
+                fast=fast.next;
+               }
+               return slow;
+            }
         }
         return null;
+        // ListNode temp=head;
+        // HashSet<ListNode> hs=new HashSet<>();
+        // while(temp!=null){
+        //    if(hs.contains(temp)){
+        //      return temp;
+        //    }
+        //    hs.add(temp);
+        //    temp=temp.next;
+        // }
+        // return null;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
