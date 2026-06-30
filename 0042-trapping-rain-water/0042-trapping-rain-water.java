c9250793a2a8582d9ad16e3a8bf58e609eb3c3b0
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-30 10:40:07 +0530

    Time: 0 ms (100%), Space: 47.7 MB (68.51%) - LeetHub - Time: O(n), Space: O(1)

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,20 +1,57 @@
 class Solution {
+    public int[] prefixmax(int[] arr){
+        int[] ans=new int[arr.length];
+        ans[0]=arr[0];
+        for(int i=1;i<arr.length;i++){
+            ans[i]=Math.max(ans[i-1],arr[i]);
+        }
+        return ans;
+    }
+    public int[] suffixmax(int[] arr){
+        int[] ans=new int[arr.length];
+        ans[arr.length-1]=arr[arr.length-1];
+        for(int i=arr.length-2;i>=0;i--){
+            ans[i]=Math.max(ans[i+1],arr[i]);
+        }
+        return ans;
+    }
     public int trap(int[] height) {
+    // int[] leftmax=prefixmax(height);
+    // int[] rightmax=suffixmax(height);
+    // int c=0;
+    // for(int i=0;i<height.length;i++){
+    //     int val=Math.min(leftmax[i],rightmax[i]);
+    //     c+=(val-height[i]);
+    // }
+    // return c;
+    
+    // o(n)-jl
         int l=0;
       int r=height.length-1;
       int ans=0;
       int leftmax=0,rightmax=0;
       while(l<r){
-        leftmax=Math.max(leftmax,height[l]);
-        rightmax=Math.max(rightmax,height[r]);
-        if(leftmax<rightmax){
-            ans+=Math.abs(height[l]-leftmax);
-            l++;
+      
+        if(height[l]<=height[r]){
+           if(height[l]<leftmax){
+            ans+=(leftmax-height[l]);
+           }else{
+            leftmax=height[l];
+           }
+           l++;
         }else{
-            ans+=Math.abs(height[r]-rightmax);
-            r--;
+           if(height[r]<rightmax){
+            ans+=rightmax-height[r];
+           }else{
+            rightmax=height[r];
+           }
+           r--;
         }
       }  
       return ans;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
