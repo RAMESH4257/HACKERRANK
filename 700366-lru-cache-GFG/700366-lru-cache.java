056f2b38ad883cabcaacfae872faa18d43aa14b0
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-07-05 12:24:02 +0530

    Added solution - LeetHub

---
+++ 700366-lru-cache-GFG/700366-lru-cache.java
@@ -0,0 +1,81 @@
+class Node{
+    public int key,val;
+    public Node next,prev;
+    Node(){
+        key=val=-1;
+        next=prev=null;
+    }
+    Node(int k,int v){
+       key=k;
+       val=v;
+       next=null;
+       prev=null;
+    }
+}
+class LRUCache {
+    static int capacity;
+     static Map<Integer,Node> hm;
+     static Node head,tail;
+     static void deleteNode(Node temp){
+        Node front=temp.next;
+        Node back=temp.prev;
+        back.next=front;
+        front.prev=back;
+    }
+    static void insertAfterHead(Node temp){
+        Node nextnode=head.next;
+        head.next=temp;
+        temp.next=nextnode;
+        temp.prev=head;
+        nextnode.prev=temp;
+
+    }
+
+    LRUCache(int cap) {
+        capacity=cap;
+        hm=new HashMap<>();
+        head=new Node();
+        tail=new Node();
+        head.next=tail;
+        tail.prev=head;
+        
+    }
+
+    public static int get(int key) {
+         if(!hm.containsKey(key))
+            return -1;
+        Node node=hm.get(key);
+        int ans=node.val;
+        deleteNode(node);
+        insertAfterHead(node);
+        return ans;
+    }
+
+        
+    public static void put(int key, int value) {
+        if(hm.containsKey(key)){
+            Node n=hm.get(key);
+            n.val=value;
+            deleteNode(n);
+            insertAfterHead(n);
+            return;
+        }
+        if(hm.size()==capacity){
+            Node n=tail.prev;
+            hm.remove(n.key);
+            deleteNode(n);
+            Node newnode=new Node(key,value);
+            hm.put(key,newnode);
+            insertAfterHead(newnode);
+            return;
+
+        }
+         Node newnode=new Node(key,value);
+        hm.put(key,newnode);
+        insertAfterHead(newnode);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
